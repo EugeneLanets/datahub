d9d9be01a9cdfcc12fbf3f3c95062e509b01691f
Eugene Lanets <swarrt@yandex.ru>
2026-09-14 16:52:57 +0300

    task-done

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\StatusController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\StatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
